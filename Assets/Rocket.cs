fba165c7ee3f62b6de214cddd0d065e59f5dce98
MW <waiting4marks@gmail.com>
2020-05-02 18:28:59 +0200

    43 Basic Input Binding first c# script

---
+++ Assets/Rocket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up);
+            print("power");
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            rigidbody.AddRelativeForce(Vector3.left);
+
+            print("lewo");
+        } else if(Input.GetKey(KeyCode.D)) {
+            rigidbody.AddRelativeForce(Vector3.right);
+            print("prawo");
+        }
+    }
+}
